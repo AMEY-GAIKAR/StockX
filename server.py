6b9a9c9abdc9b6c1020dbb826a43e86477a1992d
Amey <ameygaikar28@gmail.com>
2023-03-28 20:19:46 +0530

    Imporove documentation and fix return paths

--- server.py
+++ server.py
@@ -1,13 +1,3 @@
-alphavantage_apiendpoint = "https://www.alphavantage.co/query?"
-alphavantage_apikey = "KE7MBHHWS14Q5O95"
-
-newsAPI_endpoint = 'https://newsapi.org/v2/everything?'
-newsAPI_key = 'e566609b3a074d10891c172ccbcdcd62'
-
-STOCKS = ['GOOGL', 'TSLA']
-GRAPHS = [] #for storing all objects 
-ARTICLES = []
-
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -19,18 +9,30 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.offline as pyo  
 
+alphavantage_apiendpoint = "https://www.alphavantage.co/query?"
+alphavantage_apikey = "KE7MBHHWS14Q5O95"
 
+newsAPI_endpoint = 'https://newsapi.org/v2/everything?'
+newsAPI_key = 'e566609b3a074d10891c172ccbcdcd62'
+
+STOCKS = ['GOOGL', 'TSLA']    #Inital Stocks to display
+GRAPHS = []    #for storing all objects 
+ARTICLES = []    #for storing articles
+
+
+#WTFORM to add to list STOCKS
 class AddForm(FlaskForm):
     stock = StringField(label='name', validators=[DataRequired()])
     submit = SubmitField(label='Submit')
 
 
+#Class to create Graph object
 class Graph():
     def __init__(self, name, df):
         self.name = name
         self.df = df
         self.process()  #Calculates Current Price, Difference and converts to datetime 
-        self.create_color() #sets self.color to green or red depending on DELTA 
+        self.create_color() #sets color to green or red depending on DELTA 
     
     def process(self):
         self.df['timestamp'] = pd.to_datetime(self.df['timestamp']) #Converts timestamp to datetime type
@@ -44,15 +46,29 @@ class Graph():
             self.color = 'red'
     
     def line(self):
-        self.line = px.line(data_frame=self.df, x=self.df['timestamp'], y=self.df['open'], labels={'x':'', 'y':'USD'}, title=f'{self.name}: {self.DELTA}%   Current Price:${self.df["open"][0]}', hover_data=['volume'], template="plotly_dark")
+        '''Create line graph and saves it as a html file in assets folder'''
+        self.line = px.line(data_frame=self.df, 
+                            x=self.df['timestamp'], 
+                            y=self.df['open'], 
+                            labels={'x':'', 'y':'USD'}, 
+                            title=f'{self.name}: {self.DELTA}%   Current Price:${self.df["open"][0]}', 
+                            hover_data=['volume'], 
+                            template="plotly_dark")
         pyo.plot(self.line, filename=rf'templates\{self.name}line.html')
 
     def candle(self):
-        self.candle = go.Figure(data=[go.Candlestick(x=self.df['timestamp'],open=self.df['open'],high=self.df['high'],low=self.df['low'],close=self.df['close'])])
+        '''Create candlestick and saves it as a html file in assets folder'''
+        self.candle = go.Figure(data=[go.Candlestick(
+            x=self.df['timestamp'],
+            open=self.df['open'],
+            high=self.df['high'],
+            low=self.df['low'],
+            close=self.df['close'])])
         pyo.plot(self.candle, filename=rf'templates\{self.name}candle.html')
 
         
 def fetch_data(stock):
+    '''GET request to alphavantage API to download csv and save it in assets'''
     parameters_daily = {
     'function': 'TIME_SERIES_DAILY_ADJUSTED',
     'symbol': stock,
@@ -65,11 +81,13 @@ def fetch_data(stock):
         file.write(data_daily)
 
 def create_obj(stock):
+    '''Reads the csv files and creates Graph object then appends to list GRAPHS'''
     df = pd.read_csv(rf'static\assets\csv\{stock}.csv')
     graph = Graph(df=df, name=stock)
     GRAPHS.append(graph)
 
 def fetch_news(stock):
+    '''GET request to newsAPI to fetch news articles and append them to list ARTICLES as json objects'''
     news_params = {
         'apiKey': newsAPI_key,
         'q': stock,
@@ -80,20 +98,22 @@ def fetch_news(stock):
     ARTICLES.append(news)
 
 def initialize():
+    '''Downloads csv, creates Graph object and fetches news articles for all items in list STOCKS'''
     for stock in STOCKS:
         #fetch_data(stock)
         create_obj(stock)
         fetch_news(stock)
 
 def singular_initialize(stock):
+    '''Downloads csv, creates Graph object and fetches news articles for the passed argument'''
     fetch_data(stock)
     create_obj(stock)
     fetch_news(stock)
 
-initialize()
+initialize()    
 
-app = Flask(__name__)
-Bootstrap(app)
+app = Flask(__name__)    #Initiate Flask server
+Bootstrap(app)    #Add Bootstrap to server
 app.config['SECRET_KEY'] = '1j123h21ndu1jd19iackoac93328mcwnejd'
 
 @app.route('/')
@@ -105,14 +125,14 @@ def line(name):
     for graph in GRAPHS:
         if graph.name == name:
             graph.line()
-            return render_template(f'{graph.name}line.html')
+            return redirect(url_for('home'))
 
 @app.route('/candle/<name>')    
 def candle(name):
     for graph in GRAPHS:
         if graph.name == name:
             graph.candle()
-            return render_template(f'{graph.name}candle.html')
+            return redirect(url_for('home'))
         
 @app.route('/add', methods=['POST', 'GET'])
 def add():
